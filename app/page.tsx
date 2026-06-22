199628db5e0c6ec0532640d94798c065a28c8cef
Shaurya Singh <shauryasingh0302@icloud.com>
2026-06-22 18:44:06 +0530

    feat: add button component and utility functions; update dependencies
    - Introduced a new Button component with variants and sizes using class-variance-authority.
    - Added utility function `cn` for merging class names with tailwind-merge.
    - Created components.json for configuration with shadcn and radix-ui.
    - Implemented middleware for Clerk authentication in proxy.ts.
    - Updated package.json to include new dependencies: @clerk/nextjs, class-variance-authority, clsx, lucide-react, radix-ui, shadcn, tailwind-merge, and tw-animate-css.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <h1></h1>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-200">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
